bcce9d5ac1ba023bd786e2c492fb0d591af81ab2
Emma Barber <ecbarber1@gmail.com>
2013-11-03 05:31:49 -0500

    canvas resizes, but not drawing

--- script.js
+++ script.js
@@ -13,6 +13,9 @@ $(document).ready(function() {
 function init() {
 	
 	//Initialize stuff
+	$("#viewport").attr("width",$(window).get(0).innerWidth);
+    $("#viewport").attr("height",$(window).get(0).innerHeight);
+	
 	sys = arbor.ParticleSystem(1000,400,1);
 	sys.parameters({gravity:true});
 	sys.renderer = Renderer("#viewport");
@@ -21,17 +24,17 @@ function init() {
 
 	//Do some basic drawing as a test
 	var data = {
-				nodes:{
-					animals:{'color':'red','shape':'dot','label':'Animals'},
-					dog:{'color':'green','shape':'dot','label':'dog'},
-					cat:{'color':'blue','shape':'dot','label':'cat'}
-				},
-				edges:{
-					animals:{ dog:{}, cat:{} }
-				}
-			};
-			sys.graft(data);
-
+		nodes:{
+			animals:{'color':'red','shape':'dot','label':'Animals'},
+			dog:{'color':'green','shape':'dot','label':'dog'},
+			cat:{'color':'blue','shape':'dot','label':'cat'}
+		},
+		edges:{
+			animals:{ dog:{}, cat:{} }
+		}
+	};
+	sys.graft(data);
+	//clear data somehow?
 }
 
 //Bind event handler for change in base artist
@@ -45,16 +48,43 @@ $("#navbar input[name=artistName]").change(function() {
 	})
 
 	//add root
-	sys.addNode(rootName, {'color':'red','shape':'dot','label':rootName});
+	//sys.addNode(rootName, {'color':'red','shape':'dot','label':rootName});
+	//data.nodes.window[rootName].data = {'color':'red','shape':'dot','label':rootName};
+	var data = {
+		nodes:{
+			root:{'color':'red','shape':'dot','label':rootName},
+			c1:{'color':'blue','shape':'dot','label':'c1'},
+			c2:{'color':'blue','shape':'dot','label':'c2'},
+			c3:{'color':'blue','shape':'dot','label':'c3'},
+			c4:{'color':'blue','shape':'dot','label':'c4'},
+			c5:{'color':'blue','shape':'dot','label':'c5'}
+		},
+		edges:{
+			root:{c1:{},c2:{},c3:{},c4:{},c5:{}}
+		}
+	}
+
 	//add children of root
 	echonest.artist(rootName).similar(function(similarCollection){
 		
 		jQuery.each(similarCollection.getData(), function(count,item) {
 			//alert(item.name);
-			sys.addNode(item.name, {'color':'blue','shape':'dot','label':item.name});
-			sys.addEdge(rootName, item.name);
+			//sys.addNode(item.name, {'color':'blue','shape':'dot','label':item.name});
+			data.nodes["c" + (count + 1)].label = item.name;
+			//sys.addEdge(rootName, item.name);
 		})
 		
 	}, {results: 5});
 
+	sys.graft(data);
+
+})
+
+//Bind event handler for resizing
+
+$(window).resize(function(){
+	$("#viewport").attr("width",$(window).get(0).innerWidth);
+    $("#viewport").attr("height",$(window).get(0).innerHeight);
+
+    //should redraw after this
 })
