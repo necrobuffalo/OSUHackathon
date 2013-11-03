2a584d352d5aee8c5d410a656c31e024561580bc
Emma Barber <ecbarber1@gmail.com>
2013-11-03 02:28:54 -0500

    Changes tree but doesn't display children yet

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ var rootName;
 //Creates a new web/particle system
 var sys;
 
+//echonest object for api calls
+var echonest;
+
 $(document).ready(function() {
 	init();
 })
@@ -13,14 +16,8 @@ function init() {
 	sys = arbor.ParticleSystem(1000,400,1);
 	sys.parameters({gravity:true});
 	sys.renderer = Renderer("#viewport");
+	echonest = new EchoNest("9NQYW4WQCM3URUORT");
 	
-	//Bind event handler for change in base artist
-	$("#navbar input[name=artistName]").change(function() {
-		//Update root name
-		rootName = $("#navbar input[name=artistName]").val();
-		
-
-	})
 
 	//Do some basic drawing as a test
 	var data = {
@@ -36,3 +33,25 @@ function init() {
 			sys.graft(data);
 
 }
+
+//Bind event handler for change in base artist
+$("#navbar input[name=artistName]").change(function() {
+	//Update root name
+	rootName = $("#navbar input[name=artistName]").val();
+		
+	//clear old arbor data
+	sys.prune(function(node, from, to) {
+		return true;
+	})
+
+	//add root
+	sys.addNode(rootName, {'color':'red','shape':'dot','label':rootName});
+	//add children of root
+	echonest.artist(rootName).similar(function(similarCollection){
+		$.each(similarCollection.getData(), function(count,item) {
+			sys.addNode(item.name, {'color':'blue','shape':'dot','label':item.name});
+			sys.addEdge(rootName, item.name, {});
+		})
+	}, {results: 5});
+
+})
