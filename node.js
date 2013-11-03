194735eb85744e0cbfd3e3e283ebc8c5584ac491
Emma Barber <ecbarber1@gmail.com>
2013-11-02 21:27:54 -0400

    Refined node.js logic

--- node.js
+++ node.js
@@ -4,33 +4,46 @@
  * and open the template in the editor.
  */
 
-//Name of the artist
-//var name;
-//Array of the nodes for similar artists
-//var similarArtists;
-//Boolean which determines whether this will be rendered
-//var isVisible;
-
 //Constructor for Node
 function Node(name){
-    //this = new Object();
-
     //Properties
     this.name = name;
     this.similarArtists = [];
-    this.isVisible = false;
-
-    //Methods
+    this.childrenAreVisible = false;
 }
 
-//Expands the node. Gives it 5 similar artists.
+//Expands the node. Gives it 5 similar artists and toggles visibility.
 Node.prototype.expand = function(){
-    for(i = 0; i < 5; i++){
-        iName = ""; //Insert echonest code here
-        iNode = new Node(iName); //I'm not sure if this is how it works in js
-        iNode.isVisible = true;
-        this.similarArtist[i] = iNode;
+
+    //If similarArtists is not empty, no reason to populate it.
+    if (this.similarArtists.length > 0) {
+
     }
+    else {
+
+        //If similarArtists is empty, populate it.
+        for(i = 0; i < 5; i++){
+            //Add similar artists
+            this.similarArtists[i] = new Node(); //TODO populate with name from echonest
+
+            //this.similarArtists[i].node = new Node(iName); //I'm not sure if this is how it works in js //Doesn't need to happen unless we want to expand something.
+
+            //Toggle visibility
+            this.similarArtists[i] = iNode;
+        }
+    }
+
+    //Toggle visibility on, regardless of what happens.
+    this.childrenAreVisible = true;
+
+}
+
+//Collapse node, toggling visibility off.
+Node.prototype.collapse = function() {
+    //Not deleting similar artists, might want to later depending on resource usage?
+
+    //Toggle visibility off.
+    this.childrenAreVisible = false;
 }
 
 /*
