88c817bc16575b1e83f8a7c7dd3dcfe4466f9b89
Emma Barber <ecbarber1@gmail.com>
2013-11-03 07:42:15 -0500

    clicking child node makes that node the root

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ function init() {
 	//Initialize stuff
 	$("#viewport").attr("width",$(window).get(0).innerWidth);
     $("#viewport").attr("height",$(window).get(0).innerHeight);
-	
+
 	sys = arbor.ParticleSystem(1000,400,1);
 	sys.parameters({gravity:true});
 	sys.renderer = Renderer("#viewport");
@@ -23,21 +23,11 @@ function init() {
 	
 
 	//Do some basic drawing as a test
-	var data = {
-		nodes:{
-			animals:{'color':'red','shape':'dot','label':'Animals'},
-			dog:{'color':'green','shape':'dot','label':'dog'},
-			cat:{'color':'blue','shape':'dot','label':'cat'}
-		},
-		edges:{
-			animals:{ dog:{}, cat:{} }
-		}
-	};
-	sys.graft(data);
-	//clear data somehow?
+	populate();
 }
 
 //Bind event handler for change in base artist
+/*
 $("#navbar input[name=artistName]").change(function() {
 	//Update root name
 	rootName = $("#navbar input[name=artistName]").val();
@@ -47,38 +37,10 @@ $("#navbar input[name=artistName]").change(function() {
 		return true;
 	})
 
-	//add root
-	//sys.addNode(rootName, {'color':'red','shape':'dot','label':rootName});
-	//data.nodes.window[rootName].data = {'color':'red','shape':'dot','label':rootName};
-	var data = {
-		nodes:{
-			root:{'color':'red','shape':'dot','label':rootName},
-			c1:{'color':'blue','shape':'dot','label':'c1'},
-			c2:{'color':'blue','shape':'dot','label':'c2'},
-			c3:{'color':'blue','shape':'dot','label':'c3'},
-			c4:{'color':'blue','shape':'dot','label':'c4'},
-			c5:{'color':'blue','shape':'dot','label':'c5'}
-		},
-		edges:{
-			root:{c1:{},c2:{},c3:{},c4:{},c5:{}}
-		}
-	}
-
-	//add children of root
-	echonest.artist(rootName).similar(function(similarCollection){
-		
-		jQuery.each(similarCollection.getData(), function(count,item) {
-			//alert(item.name);
-			//sys.addNode(item.name, {'color':'blue','shape':'dot','label':item.name});
-			data.nodes["c" + (count + 1)].label = item.name;
-			//sys.addEdge(rootName, item.name);
-		})
-		
-	}, {results: 5});
-
-	sys.graft(data);
+	populate();
 
 })
+*/
 
 //Bind event handler for resizing
 
@@ -88,3 +50,42 @@ $(window).resize(function(){
 
     //should redraw after this
 })
+
+function populate() {
+	//add root
+	var data = {
+		nodes:{
+			root:{'color':'red','shape':'dot','label':getParameterByName("artistName")},
+			c1:{'color':'blue','label':'c1', 'link':'placeholder'},
+			c2:{'color':'blue','label':'c2', 'link':'placeholder'},
+			c3:{'color':'blue','label':'c3', 'link':'placeholder'},
+			c4:{'color':'blue','label':'c4', 'link':'placeholder'},
+			c5:{'color':'blue','label':'c5', 'link':'placeholder'}
+		},
+		edges:{
+			root:{c1:{},c2:{},c3:{},c4:{},c5:{}}
+		}
+	}
+
+	//add children of root
+	echonest.artist(getParameterByName("artistName")).similar(function(similarCollection){
+		
+		jQuery.each(similarCollection.getData(), function(count,item) {
+			//alert(item.name);
+			//sys.addNode(item.name, {'color':'blue','shape':'dot','label':item.name});
+			data.nodes["c" + (count + 1)].label = item.name;
+			data.nodes["c" + (count + 1)].link = "index.html?artistName=" + item.name.split(' ').join('+');
+			//sys.addEdge(rootName, item.name);
+		})
+		
+	}, {results: 5});
+
+	sys.graft(data);
+}
+
+function getParameterByName(name) {
+    name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+        results = regex.exec(location.search);
+    return results == null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
