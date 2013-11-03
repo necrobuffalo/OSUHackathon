4fa1428e2219599bfa7decf0c75dd14e78315cca
Emma Barber <ecbarber1@gmail.com>
2013-11-03 01:11:14 -0500

    adding basic arbor.js support

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //Artist name provided by user
 var rootName;
-//Creates a new web
-var web;
+//Creates a new web/particle system
+var sys;
 
 $(document).ready(function() {
 	init();
@@ -10,25 +10,29 @@ $(document).ready(function() {
 function init() {
 	
 	//Initialize stuff
-	web = new Web(rootName);
-	web.root.expand();
+	sys = arbor.ParticleSystem(1000,400,1);
+	sys.parameters({gravity:true});
+	sys.renderer = Renderer("#viewport");
 	
 	//Bind event handler for change in base artist
 	$("#navbar input[name=artistName]").change(function() {
 		//Update root name
 		rootName = $("#navbar input[name=artistName]").val();
 		
-		//Update the web
-		web = new Web(rootName);
-		web.root.expand();
-		
-		//Render the web
-		web.render();
+
 	})
 
+	//Do some basic drawing as a test
+	var data = {
+				nodes:{
+					animals:{'color':'red','shape':'dot','label':'Animals'},
+					dog:{'color':'green','shape':'dot','label':'dog'},
+					cat:{'color':'blue','shape':'dot','label':'cat'}
+				},
+				edges:{
+					animals:{ dog:{}, cat:{} }
+				}
+			};
+			sys.graft(data);
 
-}
-
-function draw() {
-	web.render();
 }
